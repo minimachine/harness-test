0d8041c6e6b28cc4563210189bffe681801f12cb
minimachine <marcie.sellers@gmail.com>
2023-06-10 20:14:50 -0500

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,1 +1,24 @@
+terraform {
+  required_providers {
+    harness = {
+      source = "harness/harness"
+    }
+  }
+}
 
+# #Configure the Harness provider for First Gen resources
+# provider "harness" {
+#   endpoint   = "https://app.harness.io/gateway"
+#   account_id = "...."
+#   api_key    = "......"
+# }
+
+resource "harness_application" "myapp" {
+  name        = "My Application"
+  description = "This is my first Harness application"
+
+  tags = [
+    "mytag:myvalue",
+    "team:development"
+  ]
+}
